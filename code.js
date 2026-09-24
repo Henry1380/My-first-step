96838d73e3be6371e124e9d381529dc4664acd3b
Henry1380 <henry.nguyen@lerniastudent.se>
2026-09-24 22:09:16 +0200

    Finishing touches

---
+++ code.js
@@ -0,0 +1,7 @@
+const title = document.getElementById("title");
+
+const button = document.getElementById("myButton");
+
+button.addEventListener("click", () => {
+    title.textContent = "Goodbye World!"; 
+});
